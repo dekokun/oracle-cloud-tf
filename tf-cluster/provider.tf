f41d07d8875b978f9aafe0ab469410b7239d8e25
dekokun <dekokun@hatena.ne.jp>
2021-12-31 21:08:22 +0900

    add oks cluster

---
+++ tf-cluster/provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaai7sj37slvzwe5oxju5xlvcaefq2zz6numoghsxect2yd5w2tg5nq"
+  user_ocid        = "ocid1.user.oc1..aaaaaaaa6ia2srogbme22gfnu5ejqf4erdfj4ikx4tzw3cgj2b5bcmh6w6ra"
+  private_key_path = "~/.oci/dekokun.pem"
+  fingerprint      = "2d:49:c7:77:bb:dd:d0:aa:63:90:58:d9:79:4d:d1:d4"
+  region           = "ap-tokyo-1"
+}
---
+++ tf-cluster/vcn-module.tf
@@ -0,0 +1,18 @@
+# Source from https://registry.terraform.io/modules/oracle-terraform-modules/vcn/oci/
+
+module "vcn" {
+  source  = "oracle-terraform-modules/vcn/oci"
+  version = "3.1.0"
+  # insert the 4 required variables here
+  # Required
+  compartment_id = oci_identity_compartment.tf-compartment.id
+  region = "ap-tokyo-1"
+  vcn_name = "bitcoin"
+  vcn_dns_label = "bitcoin"
+
+  # Optional
+  create_internet_gateway = true
+  create_nat_gateway = true
+  create_service_gateway = true
+  vcn_cidrs = tolist(["10.0.0.0/16"])
+}
