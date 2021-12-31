a36b64342713a1bd3fa3f16557594f5ed6db583f
dekokun <dekokun@hatena.ne.jp>
2021-12-31 11:10:29 +0900

    add public security rule

--- tf-vcn/outputs.tf
+++ tf-vcn/outputs.tf
@@ -25,3 +25,12 @@ output "private-security-list-name" {
 output "private-security-list-OCID" {
   value = oci_core_security_list.private-security-list.id
 }
+
+# Outputs for public security list
+
+output "public-security-list-name" {
+  value = oci_core_security_list.public-security-list.display_name
+}
+output "public-security-list-OCID" {
+  value = oci_core_security_list.public-security-list.id
+}
