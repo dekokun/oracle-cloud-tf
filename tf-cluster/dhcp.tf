36e1fe9aba793ef424e455c4872e21c81d7eb987
dekokun <dekokun@hatena.ne.jp>
2022-01-01 13:35:49 +0900

    formatting

--- tf-cluster/dhcp.tf
+++ tf-cluster/dhcp.tf
@@ -1,14 +1,14 @@
 # Source from https://registry.terraform.io/providers/hashicorp/oci/latest/docs/resources/core_dhcp_options
 
-resource "oci_core_dhcp_options" "dhcp-options"{
+resource "oci_core_dhcp_options" "dhcp-options" {
 
   # Required
   compartment_id = oci_identity_compartment.tf-compartment.id
-  vcn_id = module.vcn.vcn_id
+  vcn_id         = module.vcn.vcn_id
   #Options for type are either "DomainNameServer" or "SearchDomain"
   options {
-      type = "DomainNameServer"
-      server_type = "VcnLocalPlusInternet"
+    type        = "DomainNameServer"
+    server_type = "VcnLocalPlusInternet"
   }
 
   # Optional
--- tf-cluster/public-security-list.tf
+++ tf-cluster/public-security-list.tf
@@ -1,59 +1,57 @@
 # Source from https://registry.terraform.io/providers/hashicorp/oci/latest/docs/resources/core_security_list
 
-resource "oci_core_security_list" "public-security-list"{
+resource "oci_core_security_list" "public-security-list" {
 
-    # Required
-    compartment_id = oci_identity_compartment.tf-compartment.id
-    vcn_id = module.vcn.vcn_id
+  # Required
+  compartment_id = oci_identity_compartment.tf-compartment.id
+  vcn_id         = module.vcn.vcn_id
 
-    # Optional
-    display_name = "crypt-public-subnet"
+  # Optional
+  display_name = "crypt-public-subnet"
 
-    egress_security_rules {
-        stateless = false
-        destination = "0.0.0.0/0"
-        destination_type = "CIDR_BLOCK"
-        protocol = "all"
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all"
+  }
+
+  ingress_security_rules {
+    display_name = "k8s api"
+    stateless    = false
+    source       = "0.0.0.0/0"
+    source_type  = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+    protocol = "6"
+    tcp_options {
+      min = 6443
+      max = 6443
     }
+  }
+  ingress_security_rules {
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1
+    protocol = "1"
 
-    ingress_security_rules {
-        display_name = "k8s api"
-        stateless = false
-        source = "0.0.0.0/0"
-        source_type = "CIDR_BLOCK"
-        # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
-        protocol = "6"
-        tcp_options {
-            min = 6443
-            max = 6443
-        }
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+      code = 4
     }
+  }
+  ingress_security_rules {
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1
+    protocol = "1"
 
-    ingress_security_rules {
-        stateless = false
-        source = "0.0.0.0/0"
-        source_type = "CIDR_BLOCK"
-        # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1
-        protocol = "1"
-
-        # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
-        icmp_options {
-          type = 3
-          code = 4
-        }
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
     }
-
-    ingress_security_rules {
-        stateless = false
-        source = "0.0.0.0/0"
-        source_type = "CIDR_BLOCK"
-        # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1
-        protocol = "1"
-
-        # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
-        icmp_options {
-          type = 3
-        }
-    }
+  }
 
 }
